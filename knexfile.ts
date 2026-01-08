443e4695648b968a4c0dab79aa31cc87af044df5
Marcelle <marcellealves.dev@gmail.com>
2026-01-08 20:18:52 -0300

    install `knex` and `sqlite3` and creates `knexfile.ts` file and config

---
+++ knexfile.ts
@@ -0,0 +1,21 @@
+export default {
+  client: "sqlite3",
+  connection: {
+    filename: "./scr/database/database.db"
+  },
+  useNullAsDefault: true,
+  pool: {
+    afterCreate: (connection: any, done: any) => {
+      connection.run("PRAGMA foreign_keys = ON")
+      done()
+    }
+  },
+  migrations: {
+    extensions: "ts",
+    directory: "./src/database/migrations"
+  },
+  seeds: {
+    extensions: "ts",
+    directory: "./src/database/seeds"
+  }
+}
